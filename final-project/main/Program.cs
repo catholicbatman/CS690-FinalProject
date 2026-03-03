3bafe6d65d0f2a1246c05fecd0857ee224fe3083
catholicbatman <dannemiller.29@protonmail.com>
2026-03-03 22:27:34 +0000

    Add ansiconsole package and basic menu

--- final-project/main/Program.cs
+++ final-project/main/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+    
+        string choice;
+        do {
+            Console.WriteLine("What would you like to do?");
+            choice = Console.ReadLine();
+            Console.WriteLine(choice);
+        } while (choice != "Exit");
     }
 }
