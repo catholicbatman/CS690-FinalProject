7f32eecc039a5a04d425f2a2654196853efcfa66
catholicbatman <dannemiller.29@protonmail.com>
2026-03-09 20:25:25 +0000

    Minor changes

--- final-project/main/Program.cs
+++ final-project/main/Program.cs
@@ -8,12 +8,19 @@ class Program
     
         string choice;
         do {
+
+            //This is the main menu for selection by presenting choices
             choice = AnsiConsole.Prompt(
             new SelectionPrompt<string>()
             .Title("What would you like to do?")
             .AddChoices("Manage Supply Inventory","Track Medical Information","Track Exercise","Control Meal Records","Track Dog Info","Exit")
             );
-            Console.WriteLine(choice);
+
+
+            if(choice == "Manage Supply Inventory")
+            {
+                Console.WriteLine("Manage Supply");
+            }
         } while (choice != "Exit");
     }
 }
