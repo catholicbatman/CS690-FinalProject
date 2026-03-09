9a13b551d3d16d68d6d44317317e1204f38ea197
catholicbatman <dannemiller.29@protonmail.com>
2026-03-09 22:31:03 +0000

    Work in progress on menus

--- final-project/main/Program.cs
+++ final-project/main/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace main;
 
+using System.Reflection.Metadata.Ecma335;
 using Spectre.Console;
 class Program
 {
@@ -15,11 +16,71 @@ class Program
             .Title("What would you like to do?")
             .AddChoices("Manage Supply Inventory","Track Medical Information","Track Exercise","Control Meal Records","Track Dog Info","Exit")
             );
-
-
+            
+            //submenu for selection supply things
             if(choice == "Manage Supply Inventory")
             {
                 Console.WriteLine("Manage Supply");
+            }
+
+            //submenu for selecting just medical things
+            else if(choice == "Track Medical Information")
+            {
+                string medModeChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Which would you like to manage?")
+                .AddChoices("Appointments","Medications","Vaccinations","Exit")
+                );
+
+                    //sub submenu selection for appointment options
+                    if (medModeChoice == "Appointments")
+                    {
+                    do {
+                        string AppointmentChoice = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("What would you like to do?")
+                        .AddChoices("View Appointments","Add Appointment","Mark Appointment Complete","Remove Appointment", "Return to medical menu")
+                        );
+
+                            if (AppointmentChoice == "View Appointments")
+                            {
+                            Console.WriteLine("Appointments Viewed");
+                            }
+                            else if (AppointmentChoice == "Add Appointment")
+                            {
+                            Console.WriteLine("Added Appointment");
+                            }
+                            else if (AppointmentChoice == "Mark Appointment Complete")
+                            {
+                            Console.WriteLine("Appointment Completed");
+                            }
+                            else if (AppointmentChoice == "Remove Appointment")
+                            {
+                            Console.WriteLine("Appointment Removed");
+                            }
+                    }while(Appointmentchoice != "Return to medical menu");        
+                    }
+
+                    //sub submenu selection for medication options
+                    else if (medModeChoice == "Medications")
+                    {
+                        string MedicationChoice = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("What would you like to do?")
+                        .AddChoices("View Current Medications","Add Medication","Remove Medication","Show Medication Reminders", "Return to main menu")
+                        );
+                    }
+
+                    else if (medModeChoice == "Vaccinations")
+                    {
+                        string VaccinationChoice = AnsiConsole.Prompt(
+                        new SelectionPrompt<string>()
+                        .Title("What would you like to do?")
+                        .AddChoices("View Vaccination History","Show Vaccination Reminders","Record A Vaccination", "Return to main menu")
+                        );
+                    }
+
+
             }
         } while (choice != "Exit");
     }
