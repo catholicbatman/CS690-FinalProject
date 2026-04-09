e9c8f5a9fefb4c3cdd87dd45e5cca43f8f35bfef
Gabriel Dannemiller <dannemiller.29@protonmail.com>
2026-04-09 13:05:13 +0000

    Test Initialization

---
+++ final-project/Final-Project.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace Final_Project.Tests;
+
+using main;
+
+public class FileSaverTests
+{
+
+    FileSaver fileSaver;
+    string testFileName;
+
+    public FileSaverTests(){
+        testFileName = "test.txt";
+        fileSaver = new FileSaver(testFileName);
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        fileSaver.AppendLine("This is a test.");
+        string[] contents = File.ReadAllText(testFileName);
+        Assert.Equal("This is a test." +Environment.NewLine, contents);
+    }
+}
