24ebfc96ea0ba7cbf1ccc8c875cba3b2104e954e
Gabriel Dannemiller <dannemiller.29@protonmail.com>
2026-04-10 01:12:15 +0000

    Tests finished

--- final-project/Final-Project.Tests/LogCreatorTests.cs
+++ final-project/Final-Project.Tests/LogCreatorTests.cs
@@ -1,35 +1,45 @@
 namespace Final_Project.Tests;
 
 using main;
+using System.Linq;
 
 public class LogCreatorTests
 {
     LogCreator logCreator;
-    FileSaver fileSaver;
-    string testFileName;
+    FileSaver? fileSaver;
 
-    public LogCreatorTests(){
-        testFileName = "test.txt";
+    public LogCreatorTests()
+    {
         logCreator = new LogCreator();
+    }
+
+    [Fact]
+    public void MedicationsFileTest1()
+    {
+        string testFileName = "testMeds.txt";
         fileSaver = new FileSaver(testFileName);
-        fileSaver.AppendLine("Nail Fungus Cream,4");
-        fileSaver.AppendLine("Boogers,3");
-        MedicationLog testMedLog = new MedicationLog();    
-    }
-
-    [Fact]
-    public void MedicationsFileTest()
-    {
+        fileSaver.DeleteFile();
+        fileSaver = new FileSaver(testFileName);
+        fileSaver.AppendLine("Nail Fungus Cream,3");
+        fileSaver.AppendLine("Vitamin,1");
+        fileSaver.AppendLine("Benadryl,2");
         MedicationLog testMedLog = logCreator.readMedicationInfoFromFile(testFileName);
-        Medication testMed = new Medication("Nail Fungus Cream",4);
-        Assert.Contains(testMed,testMedLog.Meds);
+        Medication testMed = new Medication("Nail Fungus Cream",3);
+        Assert.Contains(testMed.Name,testMedLog.Meds[0].Name);
+        int medCount = testMedLog.Meds.Count();
+
+        Assert.Equal(3,medCount);
     }
 
     [Fact]
-    public void Test2()
+    public void WalkFileTest()
     {
-        
+        string testFileName = "testWalks.txt";
+        fileSaver = new FileSaver(testFileName);
+        fileSaver.AppendLine("04/05/2026 20:17:32 01:10:00");
+        WalkRecord testWalkRecord = logCreator.readWalkInfoFromFile(testFileName);
+        Walk testWalk = new Walk(new DateTime(2026,4,5,20,17,32),new TimeSpan(1,10,0));
+        Assert.True(testWalkRecord.Walks.Any(result => result.Date == testWalk.Date && result.WalkTime == testWalk.WalkTime));
     }
-
 
 }
